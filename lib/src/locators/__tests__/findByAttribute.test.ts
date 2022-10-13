0a4ecde1a18ddefc4e6b4957e2b16c399f741b5f
Samyog Dhital <dhitalsamyog123@gmail.com>
2022-10-13 17:30:27 +0545

    pathUrl on tsconfig

--- lib/src/locators/__tests__/findByAttribute.test.ts
+++ lib/src/locators/__tests__/findByAttribute.test.ts
@@ -1,7 +1,10 @@
 import { findByAttribute } from "../findByAttribute";
+
 describe("Find the element by attribute", () => {
   it("should find the elemnt by provided attribute", () => {
     cy.visit("https://www.w3schools.com/");
-    findByAttribute("data-name", "w3_cert_badge");
+    findByAttribute("data-name", "w3_cert_badge")
+      .should("exist")
+      .scrollIntoView();
   });
 });
